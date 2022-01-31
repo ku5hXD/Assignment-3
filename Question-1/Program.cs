3b36ef0df73c519cecf18f0a2174b3c97b5b0ff3
ku5h <kushparmar4@gmail.com>
2022-01-31 22:29:32 +0530

    initial commit

---
+++ Question-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https=//aka.ms/new-console-template for more information
+
+using System;
+
+namespace Program
+{
+    class DoTheMath
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the numerator?");
+            int numerator = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the denominator?");
+            int denominator = Int32.Parse(Console.ReadLine());
+
+            int quotient = numerator / denominator;
+            int remainder = numerator % denominator;
+
+            Console.WriteLine($"\nInteger division result = {quotient} with a remainder = {remainder}\n");
+
+            float ans = (float)numerator / denominator;
+            Console.WriteLine($"Floating point division result = {ans}\n");
+
+            Console.WriteLine($"The result as a mixed fraction is {quotient} {remainder}/{denominator}\n");
+
+        }
+    }
+
+
+}
+
---
+++ Question-2/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https=//aka.ms/new-console-template for more information
+
+using System;
+
+namespace Program
+{
+    class IsItASentence
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Sentence?");
+            string str = Console.ReadLine();
+            Console.WriteLine($"\n Length of sentence is : {str.Length}\n");
+
+            if (str[str.Length - 1] == '.')
+            {
+                Console.WriteLine("The sentence represents a declarative sentence.\n");
+            }
+            else if (str[str.Length - 1] == '?')
+            {
+                Console.WriteLine("The sentence represents a interrogatory sentence.\n");
+            }
+            else if (str[str.Length - 1] == '!')
+            {
+                Console.WriteLine("The sentence represents a exclamation sentence.\n");
+            }
+            else
+            {
+                Console.WriteLine("The string entered is not a sentence.\n");
+            }
+
+            Console.WriteLine("Enter your name?\n");
+            string name = Console.ReadLine();
+
+            int i = 0;
+            while (i < name.Length - 1 && name[i] != ' ')
+            {
+                i++;
+            }
+
+            if (i >= name.Length - 1)
+            {
+                Console.WriteLine($"{name}\n");
+            }
+            else
+            {
+                string firstName = name.Substring(0, i);
+                i++;
+                string lastName = name.Substring(i, name.Length - i);
+
+                Console.WriteLine($"\n{lastName}, {firstName}");
+            }
+        }
+    }
+
+
+}
+
